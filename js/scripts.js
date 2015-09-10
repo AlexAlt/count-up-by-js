5b5614918fa1334ba9cb6772b5bf8653dee70414
Janice Dunn & Alex Altieri <student@epicodus.com>
2015-09-10 09:52:24 -0700

    Add front-end form.

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,16 @@ var countBy = function(numberOne, numberTwo) {
   return counter;
 
 };
+
+$(document).ready(function() {
+  $("form#count-by").submit(function(event) {
+    var numberOne = parseInt($("input#number-one").val());
+    var numberTwo = parseInt($("input#number-two").val());
+    var counter = countBy(numberOne, numberTwo);
+console.log(counter)
+    $(".number-list").text(counter);
+
+    $("#result").show();
+    event.preventDefault();
+  });
+});
